089d9c456414bdbb8fc230546c16b92a27ab3877
Mark Rattle <mark.rattle@gmail.com>
2022-09-27 18:22:17 -0400

    displayed cart items on console

--- scripts.js
+++ scripts.js
@@ -1,33 +1,33 @@
-import data from './data.js';
+// import data from './data.js';
 
-const itemsContainer = document.querySelector("#items");
+// const itemsContainer = document.querySelector("#items");
 
-for (let i = 0; 1 < data.length; i++) {
-    // Create a new div element and give it a class name
-    const newDiv = document.createElement('div');
-    newDiv.className = 'item';
-    // Create an image element
-    const img = document.createElement('img');
-    // This will change each time we go through the loop.  Can you explain why?
-    img.src = data[i].image;
-    img.width = 300;
-    img.height = 300;
-    // Add the image to the div
-    newDiv.appendChild(img);
-    // console.log(img); // Check the console!
-    itemsContainer.appendChild(newDiv);
-    const desc = document.createElement('p');
-    desc.innerText = data[i].desc;
-    newDiv.appendChild(desc);
-    const price = document.createElement('p');
-    price.innerText = data[i].price;
-    newDiv.appendChild(price);
-    const button = document.createElement('button');
-    button.id = data[i].name;
-    button.dataset.price = data[i].price;
-    button.innerHTML = "Add to Cart";
-    newDiv.appendChild(button);
-}
+// for (let i = 0; 1 < data.length; i++) {
+//     // Create a new div element and give it a class name
+//     const newDiv = document.createElement('div');
+//     newDiv.className = 'item';
+//     // Create an image element
+//     const img = document.createElement('img');
+//     // This will change each time we go through the loop.  Can you explain why?
+//     img.src = data[i].image;
+//     img.width = 300;
+//     img.height = 300;
+//     // Add the image to the div
+//     newDiv.appendChild(img);
+//     // console.log(img); // Check the console!
+//     itemsContainer.appendChild(newDiv);
+//     const desc = document.createElement('p');
+//     desc.innerText = data[i].desc;
+//     newDiv.appendChild(desc);
+//     const price = document.createElement('p');
+//     price.innerText = data[i].price;
+//     newDiv.appendChild(price);
+//     const button = document.createElement('button');
+//     button.id = data[i].name;
+//     button.dataset.price = data[i].price;
+//     button.innerHTML = "Add to Cart";
+//     newDiv.appendChild(button);
+// }
 
 const cart = [];
 
@@ -38,13 +38,13 @@ function addItem(name, price) {
             return
         }
     }
-    const item = {name: name, price: price, qty: 1}
+    const item = {name, price, qty: 1}
     cart.push(item);
 }
 
 function showItems() {
     for (let i = 0; i < cart.length; i++) {
-        console.log(`- ${cart[i].name}, $${cart[i].price} x ${cart[i].qty}}`)
+        console.log(`- ${cart[i].name}, $${cart[i].price} x ${cart[i].qty}`)
     }
 }
 
