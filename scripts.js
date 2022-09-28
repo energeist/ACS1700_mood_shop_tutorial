e643f66eab3e93114d900fe7280e1ee44817f472
Mark Rattle <mark.rattle@gmail.com>
2022-09-27 21:59:17 -0400

    linked add to cart button to cart

--- scripts.js
+++ scripts.js
@@ -1,36 +1,37 @@
-// import data from './data.js';
+import data from '/data.js';
 
 const itemsContainer = document.querySelector("#items");
 const itemList = document.getElementById('item-list');
 const totalDiv = document.getElementById('total-price');
 const quantityDiv = document.getElementById('quantity-items')
 
-// for (let i = 0; 1 < data.length; i++) {
-//     // Create a new div element and give it a class name
-//     const newDiv = document.createElement('div');
-//     newDiv.className = 'item';
-//     // Create an image element
-//     const img = document.createElement('img');
-//     // This will change each time we go through the loop.  Can you explain why?
-//     img.src = data[i].image;
-//     img.width = 300;
-//     img.height = 300;
-//     // Add the image to the div
-//     newDiv.appendChild(img);
-//     // console.log(img); // Check the console!
-//     itemsContainer.appendChild(newDiv);
-//     const desc = document.createElement('p');
-//     desc.innerText = data[i].desc;
-//     newDiv.appendChild(desc);
-//     const price = document.createElement('p');
-//     price.innerText = data[i].price;
-//     newDiv.appendChild(price);
-//     const button = document.createElement('button');
-//     button.id = data[i].name;
-//     button.dataset.price = data[i].price;
-//     button.innerHTML = "Add to Cart";
-//     newDiv.appendChild(button);
-// }
+
+for (let i = 0; i < data.length; i++) {
+    // Create a new div element and give it a class name
+    const newDiv = document.createElement('div')
+    newDiv.className = 'item'
+    // Create an image element
+    const img = document.createElement('img')
+    // This will change each time we go through the loop.  Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+    // Add the image to the div
+    newDiv.appendChild(img)
+    // console.log(img); // Check the console!
+    itemsContainer.appendChild(newDiv);
+    const desc = document.createElement('p');
+    desc.innerText = data[i].desc;
+    newDiv.appendChild(desc);
+    const price = document.createElement('p');
+    price.innerText = data[i].price;
+    newDiv.appendChild(price);
+    const button = document.createElement('button');
+    button.id = data[i].name;
+    button.dataset.price = data[i].price;
+    button.innerHTML = "Add to Cart";
+    newDiv.appendChild(button);
+}
 
 const cart = [];
 
@@ -72,7 +73,8 @@ function showItems() {
         itemStr +=(`<li>${cart[i].name}, $${cart[i].price} x ${cart[i].qty} = $${(cart[i].price * cart[i].qty)} </li>`)
     }
     itemList.innerHTML = itemStr
-
+    quantityDiv.innerHTML = `You have ${getQty()} items in your cart`
+    totalDiv.innerHTML = `Your cart total is: $${getTotal()}`
 }
 
 function getQty() {
@@ -100,13 +102,19 @@ addItem('orange', 1.29)
 addItem('orange', 1.29)
 addItem('orange', 1.29)
 showItems()
-quantityDiv.innerHTML = `You have ${getQty()} items in your cart`
-totalDiv.innerHTML = `Your cart total is: $${getTotal()}`
+
 console.log(`You have ${getQty()} items in your cart`)
 showItems()
 console.log(`Your cart total is: $${getTotal()}`)
-// subtractItem('apple', 1)
-// subtractItem('orange', 3)
+subtractItem('apple', 1)
+subtractItem('orange', 3)
 console.log(`You have ${getQty()} items in your cart`)
 showItems()
 console.log(`Your cart total is: $${getTotal()}`)
+const all_items_button = Array.from(document.querySelectorAll("button"))
+console.log(all_items_button)
+
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+}))
